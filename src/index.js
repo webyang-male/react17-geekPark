3f16f1da6c7fa5dd64fc5d8fcb7b1b9eda168a95
webyang-male <mr.yangweb@gmail.com>
2022-04-11 19:06:17 +0800

    :pencil: 筛选功能完成,todo:文章删除编辑

--- src/index.js
+++ src/index.js
@@ -8,3 +8,19 @@ import "antd/dist/antd.min.css";
 import "./index.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+window.onload = function () {
+  // 手动添加mate标签
+  const addMeta = (httpEquiv, content) => {
+    const meta = document.createElement("meta");
+    meta.content = content;
+    meta.httpEquiv = httpEquiv;
+    meta.id = "meta";
+    document.getElementsByTagName("head")[0].appendChild(meta);
+  };
+
+  addMeta("Content-Security-Policy", "upgrade-insecure-requests");
+  const meta = document.querySelector("#meta");
+  console.log(meta);
+  console.log(meta.httpEquiv, meta.content);
+};
